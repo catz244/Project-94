7678943ca2b899fd05d9884ba5c770e324146c9f
catz244 <78543753+catz244@users.noreply.github.com>
2021-06-18 03:13:57 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBk_4heVlwGDYGVL-iE9j-VbAZS8kNMZGI",
+    authDomain: "kwitter-35ec2.firebaseapp.com",
+    databaseURL: "https://kwitter-35ec2-default-rtdb.firebaseio.com",
+    projectId: "kwitter-35ec2",
+    storageBucket: "kwitter-35ec2.appspot.com",
+    messagingSenderId: "544197924140",
+    appId: "1:544197924140:web:9f9bf85e7dafa5ea0f0889",
+    measurementId: "G-9Q720937NE"
+  };
+  firebase.initializeApp(firebaseConfig);
